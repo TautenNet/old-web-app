6b4489c842b8956e3959ba299755806388b02325
Tarocafe <55334852+Tarocafe@users.noreply.github.com>
2021-11-15 20:25:13 +0100

    Keywords, discussion screen, some fixes

---
+++ res/js/api.js
@@ -0,0 +1,17 @@
+var api = {
+    account: {
+        register: (username, email, password) => {
+        
+        },
+    
+        getToken: (username, password, fun) => {
+    
+        },
+
+        profile: {
+            getAvatarUrl: () => {
+
+            },
+        }
+    },
+}
--- res/js/main.js
+++ res/js/main.js
@@ -1,36 +1,42 @@
 const   $select = selector => document.querySelector(selector),
         $selectAll = selector => document.querySelectorAll(selector);
 
-var screen = {
-    switchTo: (toScreen, properties) => {
-        let fromScreen = $select("body > .screens > .showed");
-        toScreen = $select("body > .screens > ." + toScreen);
-        properties = properties || {anim: "scale", duration:300};
-        properties.duration = properties.duration || 300;
-        
-        clearTimeout(screen.currentTimeout)
+$select(".screens").setAttribute("style", null)
+$select(".floatings").setAttribute("style", null)
 
-        if (properties.duration) {
-            fromScreen.style.animation = `${properties.anim}out ${properties.duration + 5}ms ease`;
-            toScreen.style.animation = `${properties.anim}in ${properties.duration + 5}ms ease`;
-        }
-        
-        screen.currentTimeout = setTimeout(() => {
-            fromScreen.classList.remove("showed");
-        }, properties.duration)
-        toScreen.classList.add("showed");
-    },
+function signup() {
+    //screen.floating.show("inputmissing")
+    screen.floating.show("loading")
+    setTimeout(() => {
+        screen.switchTo("discussion")
+        screen.floating.hide("loading", {anim: "scale"})
+    }, 2000)
+}
 
-    floating: {
-        show: (floating, properties) => {
-            properties = properties || {anim: "scale", duration:300}
-            let floatingParent = $select("body > .floatings");
-            floating = $select("body > .floatings > ." + floating)
-            
-            floatingParent.classList.add("showed");
-            floating.classList.add("showed");
-        }
-    },
-    
-    currentTimeout: setTimeout(null, null)
+var signin = signup;
+
+document.onkeydown = event => {
+    switch(event.key) {
+        case "Escape":
+            screen.onescape();
+            break;
+    }
+};
+
+document.onclick = event => {
+    switch(event.target) {
+        case $select("body > .floatings"):
+            screen.onescape();
+            break;
+        case $select("body > .floatings > .discussionsList"):
+            screen.onescape();
+            break;
+    }
+}
+
+if (window.matchMedia('(prefers-color-scheme: dark)').media === 'not all') {
+    document.head.insertAdjacentHTML(
+      "beforeend",
+      '<link rel="stylesheet" href="res/css/dark-theme.css">',
+    );
 }
