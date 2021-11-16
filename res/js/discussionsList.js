67331e55d4ae1806d55b2cdb6763b80072ec6be3
Tarocafe <55334852+Tarocafe@users.noreply.github.com>
2021-11-16 20:35:07 +0100

    I'll rewrite the swipe thing entirely, this is the last time you can see it

--- res/js/discussionsList.js
+++ res/js/discussionsList.js
@@ -5,7 +5,7 @@ var swipeDiscussion = {
     startY: 0,
     moveY: 0,
     startDate: 0,
-    
+
     showList: () => {
 
     },
@@ -20,7 +20,7 @@ $select(".screens > .discussion").ontouchmove = event => {
         swipeDiscussion.moveY = event.changedTouches[0].pageY - swipeDiscussion.startY;
         let moveY = swipeDiscussion.moveY;
         moveY = moveY > 0 ? moveY / 10 : moveY
-        $select(".screens > .discussion").style.transform = `translateY(${moveY}px)` // "lag" is intended
+        $select(".screens > .discussion").style.transform = `translateY(${moveY}px)` 
         $select(".screens > .discussionList").style.transform = `translateY(${moveY + window.innerHeight}px)`
         //console.log(moveY)
     }
@@ -28,10 +28,13 @@ $select(".screens > .discussion").ontouchmove = event => {
 
 $select(".screens > .discussion").ontouchstart = event => {
     swipeDiscussion.startY = event.changedTouches[0].pageY;
+    swipeDiscussion.moveY = 0;
     swipeDiscussion.enabled = (window.innerHeight - 150) < swipeDiscussion.startY ? true : false;
 
     $select(".screens > .discussionList").classList.add(swipeDiscussion.enabled ? "showed" : null)
     $select(".screens > .discussionList").style.transform = `translateY(${window.innerHeight}px)`
+    $select(".screens > .discussionList").style.animation = `none`
+    $select(".screens > .discussionList").style.background = `var(--background-tone-dark)`
     $select(".screens > .discussion").style.transition = "0s"
     $select(".screens > .discussionList").style.transition = "0s"
 
@@ -48,10 +51,12 @@ $select(".screens > .discussion").ontouchend = event => {
     $select(".screens > .discussionList").style.transition = "0.3s"
     $select(".screens > .discussion").style.transform = moveY < swipeAcceptance ? `translateY(-100%)` : `translateY(0px)`
     $select(".screens > .discussionList").style.transform = moveY < swipeAcceptance ? `translateY(0px)` : `translateY(${window.innerHeight}px)`
+    $select(".screens > .discussionList").style.background = moveY < swipeAcceptance ? `var(--background-tone-normal)` : `var(--background-tone-dark)`
     swipeDiscussion
     console.log(swipeAcceptance)
 
     setTimeout(() => {
         (moveY < swipeAcceptance ? $select(".screens > .discussion") : $select(".screens > .discussionList")).classList.remove("showed")
+        $select(".screens > .discussion").style.transform = `translateY(0px)`
     }, 300)
 }
